effa9ef77983b42191c17207ca866e1e30beaf9e
Marisonia Ferrandino <s1093762@studenti.univpm.it>
2026-01-06 18:07:08 +0100

    Aggiungo login.html

--- css/js/script.js
+++ css/js/script.js
@@ -25,3 +25,50 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("loginForm");
+
+  if (form) {
+    form.addEventListener("submit", e => {
+      e.preventDefault();
+
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+      const accept = document.getElementById("accept").checked;
+      const errorMsg = document.getElementById("errorMsg");
+
+      if (!accept) {
+        errorMsg.textContent = "Devi accettare i termini";
+        return;
+      }
+
+      // 👤 UTENTI SIMULATI
+      if (email === "admin@test.com" && password === "admin123") {
+        localStorage.setItem("logged", "true");
+        localStorage.setItem("role", "admin");
+        window.location.href = "index.html";
+      } 
+      else if (email === "user@test.com" && password === "user123") {
+        localStorage.setItem("logged", "true");
+        localStorage.setItem("role", "user");
+        window.location.href = "index.html";
+      } 
+      else {
+        errorMsg.textContent = "Credenziali errate";
+      }
+    });
+  }
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const role = localStorage.getItem("role");
+
+  if (role !== "admin") {
+    document.querySelectorAll(".admin-only").forEach(el => {
+      el.style.display = "none";
+    });
+  }
+});
+function logout() {
+  localStorage.clear();
+  window.location.href = "login.html";
+}
